b3d44e5dcdb00beebca4334af310f2107378fa3f
Maxi <128001873+maxiarias86@users.noreply.github.com>
2025-06-19 10:48:33 -0300

    Creacion de clase bastracta Transaction

---
+++ src/main/java/org/example/model/Transaction.java
@@ -0,0 +1,42 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public abstract class Transaction {
+    int id;
+    double amount;
+    LocalDate date;
+
+    public Transaction() {
+
+    }
+    public Transaction(int id, double amount, LocalDate date) {
+        this.id = id;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
