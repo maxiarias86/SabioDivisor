1a0ad85e40af79038940f6c4eb3e7435d8bb3605
Maxi <128001873+maxiarias86@users.noreply.github.com>
2025-06-26 10:11:47 -0300

    Creacion de NewExpenseJFram. Configuracion para cargado de nuevo pago correcto.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) {
         LoginJFrame login = new LoginJFrame();
         login.setVisible(true);
-        UserCache userCache = UserCache.getInstance();
-        //Poner un metodo que muestre por consola usuarios y credenciales
+        UserCache userCache = UserCache.getInstance();//Cargo los usuarios en el cache
 
+        //Metodo que muestre por consola usuarios y credenciales para tenerlos a mano
         for (User user : userCache.getAllUsers().values()) {
             System.out.println("\nUsuario: "+ user.getEmail()+"\nContraseña: "+ user.getPassword());
         }
